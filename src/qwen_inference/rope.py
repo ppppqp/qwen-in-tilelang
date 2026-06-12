c98f46c46ae962feb9b14bb9c53a5b35574b6d2d
Qiping Pan <panqiping@outlook.com>
2026-06-11 20:37:49 -0700

    fix bunch of rope stuff

--- src/qwen_inference/rope.py
+++ src/qwen_inference/rope.py
@@ -1,4 +1,3 @@
-import mlx.core as mx
 import tilelang
 import tilelang.language as T
 from tilelang.jit import JITKernel, JITImpl
@@ -26,15 +25,15 @@ def rope(X, offset, BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D):
     ):
         X_local_real = T.alloc_fragment((BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D), dtype)
         X_local_imag = T.alloc_fragment((BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D), dtype)
-        O_local = T.alloc_fragment((BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D), dtype)
-        cos_basis = T.alloc_fragment((S, BLOCK_D), dtype)
-        sin_basis = T.alloc_fragment((S, BLOCK_D), dtype)
-        for i, j in T.Parallel(BLOCK_H, BLOCK_D):
-            global_row_idx = BLOCK_H * pid_h + i
-            global_col_idx = BLOCK_D * pid_d + j + offset
-            freq = T.pow(10000.0, -global_col_idx / half_D)
-            cos_basis[i, j] = T.cos(global_row_idx * freq)
-            sin_basis[i, j] = T.sin(global_row_idx * freq)
+        O_local_real = T.alloc_fragment((BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D), dtype)
+        O_local_imag = T.alloc_fragment((BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D), dtype)
+        cos_basis = T.alloc_fragment((BLOCK_S), dtype)
+        sin_basis = T.alloc_fragment((BLOCK_S), dtype)
+        for s in T.Parallel(BLOCK_S):
+            seq_idx = offset + pid_s * BLOCK_S + s
+            freq = T.pow(10000.0, -seq_idx / half_D)
+            cos_basis[s] = T.cos(seq_idx * freq)
+            sin_basis[s] = T.sin(seq_idx * freq)
 
         cos_basis = cos_basis.reshape(1, S, 1, BLOCK_D)
         sin_basis = sin_basis.reshape(1, S, 1, BLOCK_D)
@@ -72,11 +71,11 @@ def rope(X, offset, BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D):
                 X_local_imag[n_blk_id, s_blk_id, i, j] * cos_basis[i, j]
                 + X_local_real[n_blk_id, s_blk_id, i, j] * sin_basis[i, j]
             )
-            O_local[i, j] = real
-            O_local[i, j + half_D] = imag
+            O_local_real[i, j] = real
+            O_local_imag[i, j] = imag
 
         T.copy(
-            O_local,
+            O_local_real,
             O[
                 n_blk_id * BLOCK_N : (n_blk_id + 1) * BLOCK_N,
                 s_blk_id * BLOCK_S : (s_blk_id + 1) * BLOCK_S,
@@ -84,68 +83,78 @@ def rope(X, offset, BLOCK_N, BLOCK_S, BLOCK_H, BLOCK_D):
                 pid_d * BLOCK_D : (pid_d + 1) * BLOCK_D,
             ],
         )
-
-
-class RoPE:
-    def __init__(
-        self,
-        dims: int,
-        seq_len: int,
-        base: int = 10000,
-        traditional: bool = False,
-    ):
-        assert dims % 2 == 0, "dims must be even"
-        self.dims = dims
-        self.seq_len = seq_len
-        half_dims = dims // 2
-        inner = mx.arange(0, half_dims, dtype=mx.float32) / half_dims
-        freqs = mx.power(base, -inner)
-        t = mx.arange(seq_len)
-        freqs = mx.outer(t, freqs)
-        self.cos_freqs = mx.cos(freqs)
-        self.sin_freqs = mx.sin(freqs)
-        self.base = base
-        self.half_dims = half_dims
-        self.traditional = traditional
-
-    def __call__(
-        self, x: mx.array, offset: list[slice] | slice | None = None
-    ) -> mx.array:
-        N, S, H, D = x.shape
-        if offset is not None:
-            if isinstance(offset, slice):
-                assert offset.stop - offset.start == S, f"offset must be of length {S}"
-            elif isinstance(offset, list):
-                assert len(offset) == N, (
-                    f"offsets must have the same length as batch size {N}"
-                )
-                for o in offset:
-                    assert o.stop - o.start == S, f"offset must be of length {S}"
-                offset = mx.array([list(range(i.start, i.stop)) for i in offset])
-        cos_basis = (
-            self.cos_freqs[:S, :] if offset is None else self.cos_freqs[offset, :]
+        T.copy(
+            O_local_imag,
+            O[
+                n_blk_id * BLOCK_N : (n_blk_id + 1) * BLOCK_N,
+                s_blk_id * BLOCK_S : (s_blk_id + 1) * BLOCK_S,
+                pid_h * BLOCK_H : (pid_h + 1) * BLOCK_H,
+                pid_d * BLOCK_D + half_D : (pid_d + 1) * BLOCK_D + half_D,
+            ],
         )
-        sin_basis = (
-            self.sin_freqs[:S, :] if offset is None else self.sin_freqs[offset, :]
-        )
-        # reshape x: (b, s, n_heads, head_dim // 2, 2)
-        if self.traditional:
-            x = x.reshape(N, S, H, self.half_dims, 2)
-            x1 = x[..., 0]
-            x2 = x[..., 1]
-        else:
-            x1 = x[..., 0 : self.half_dims]
-            x2 = x[..., self.half_dims : self.dims]
-        # reshape basis: (1, s, 1, dims // 2, 2)
-        cos_basis = cos_basis.reshape(-1, S, 1, self.half_dims)
-        sin_basis = sin_basis.reshape(-1, S, 1, self.half_dims)
-        # manually doing complex number multiplication..
-        real = mx.multiply(x1, cos_basis) - mx.multiply(x2, sin_basis)
-        imag = mx.multiply(x2, cos_basis) + mx.multiply(x1, sin_basis)
-        if self.traditional:
-            y = mx.stack([real, imag], axis=-1)
-            y = y.reshape(N, S, H, D)
-        else:
-            y = mx.concat([real, imag], axis=-1)
-            y = y.reshape(N, S, H, D)
-        return y.astype(x.dtype)
+    return O
+
+
+# class RoPE:
+#     def __init__(
+#         self,
+#         dims: int,
+#         seq_len: int,
+#         base: int = 10000,
+#         traditional: bool = False,
+#     ):
+#         assert dims % 2 == 0, "dims must be even"
+#         self.dims = dims
+#         self.seq_len = seq_len
+#         half_dims = dims // 2
+#         inner = mx.arange(0, half_dims, dtype=mx.float32) / half_dims
+#         freqs = mx.power(base, -inner)
+#         t = mx.arange(seq_len)
+#         freqs = mx.outer(t, freqs)
+#         self.cos_freqs = mx.cos(freqs)
+#         self.sin_freqs = mx.sin(freqs)
+#         self.base = base
+#         self.half_dims = half_dims
+#         self.traditional = traditional
+
+#     def __call__(
+#         self, x: mx.array, offset: list[slice] | slice | None = None
+#     ) -> mx.array:
+#         N, S, H, D = x.shape
+#         if offset is not None:
+#             if isinstance(offset, slice):
+#                 assert offset.stop - offset.start == S, f"offset must be of length {S}"
+#             elif isinstance(offset, list):
+#                 assert len(offset) == N, (
+#                     f"offsets must have the same length as batch size {N}"
+#                 )
+#                 for o in offset:
+#                     assert o.stop - o.start == S, f"offset must be of length {S}"
+#                 offset = mx.array([list(range(i.start, i.stop)) for i in offset])
+#         cos_basis = (
+#             self.cos_freqs[:S, :] if offset is None else self.cos_freqs[offset, :]
+#         )
+#         sin_basis = (
+#             self.sin_freqs[:S, :] if offset is None else self.sin_freqs[offset, :]
+#         )
+#         # reshape x: (b, s, n_heads, head_dim // 2, 2)
+#         if self.traditional:
+#             x = x.reshape(N, S, H, self.half_dims, 2)
+#             x1 = x[..., 0]
+#             x2 = x[..., 1]
+#         else:
+#             x1 = x[..., 0 : self.half_dims]
+#             x2 = x[..., self.half_dims : self.dims]
+#         # reshape basis: (1, s, 1, dims // 2, 2)
+#         cos_basis = cos_basis.reshape(-1, S, 1, self.half_dims)
+#         sin_basis = sin_basis.reshape(-1, S, 1, self.half_dims)
+#         # manually doing complex number multiplication..
+#         real = mx.multiply(x1, cos_basis) - mx.multiply(x2, sin_basis)
+#         imag = mx.multiply(x2, cos_basis) + mx.multiply(x1, sin_basis)
+#         if self.traditional:
+#             y = mx.stack([real, imag], axis=-1)
+#             y = y.reshape(N, S, H, D)
+#         else:
+#             y = mx.concat([real, imag], axis=-1)
+#             y = y.reshape(N, S, H, D)
+#         return y.astype(x.dtype)
